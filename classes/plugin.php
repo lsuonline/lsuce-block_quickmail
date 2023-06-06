88c2a3e0322965f5b8138b518965dbca77306d67
Martin Ringwelski <der-martin85@users.noreply.github.com>
2023-06-06 13:13:20 +0200

    Update plugin.php
    changed context in moodle 4.2

--- classes/plugin.php
+++ classes/plugin.php
@@ -126,7 +126,7 @@ class block_quickmail_plugin {
         // If we're broadcasting, only allow admins.
         if ($sendtype == 'broadcast') {
             // Make sure we have the correct context (system).
-            if (get_class($context) !== 'context_system') {
+            if (get_class($context) !== 'core\context\system' && get_class($context) !== 'context_system') {
                 return false;
             }
 
@@ -135,7 +135,7 @@ class block_quickmail_plugin {
 
         // Otherwise, we're composing.
         // Make sure we have the correct context (course).
-        if (get_class($context) !== 'context_course') {
+        if (get_class($context) !== 'core\context\course' && get_class($context) !== 'context_course') {
             return false;
         }
 
